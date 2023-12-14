0bb442207c7a600fef7d515d4b48b256a018ec05
TemoTech25 <temotech25@gmail.com>
2023-12-13 19:53:04 -0800

    check

--- tr_main.c
+++ tr_main.c
@@ -1,37 +1,34 @@
-#include "shell.h" 
+#include "shell.h"
+
 /**
- * main - function for simple shell main
- * @ac: arguments ++
- * @av: Arguments 
- * return: o (success)
-*/
+ * main - Entry point for the simple shell
+ * @ac: Number of arguments
+ * @argv: Array of arguments
+ * Return: Always 0 (success)
+ */
 
 int main(int ac, char **argv)
 {
-    char *thread = NULL, **instruct = NULL;
-    int position = 0;
-    (void) ac;
-    (void) argv;
+	char *thread = NULL, **instruct = NULL;
+	int position = 0;
+	(void) ac;
+	(void) argv;
 
-    while (1)
-    {
-        thread = temoraeez_read_line();
-        if (thread == NULL)
-        {
-            if (isatty(STDIN_FILENO))
-                write(STDOUT_FILENO, "\n", 1);
-            return (position);
-        }
+	while (1)
+	{
+		thread = temoraeez_read_line();
+		if (thread == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+				return (position);
+		}
 
-        free(thread);
-        instruct = tr_tokenizer(thread);
-        if (!instruct)
-        continue;
+		free(thread);
+		instruct = tr_tokenizer(thread);
+		if (!instruct)
+		continue;
 
-        
-
-
-
-        position = tr_execute(instruct, argv);
-    }
+		position = tr_execute(instruct, argv);
+	}
 }
--- tr_strings.c
+++ tr_strings.c
@@ -1,46 +1,64 @@
 #include "shell.h"
 
+/**
+ * tr_strdup - Duplicates a string.
+ * @str: String to be duplicated.
+ *
+ * Duplicates the input string 'str'.
+ *
+ * Return: Pointer to the duplicated string on success
+ */
+
 char *tr_strdup(const char *str)
 {
-    char *tr_ptr;
-    int tr;
-    int len = 0;
+	char *tr_ptr;
+	int tr;
+	int len = 0;
 
-    if (str == NULL)
-    return (NULL);
-    while (*str != '\0')
-    {
-        len++;
-        str++;
-    }
-    str = str - len;
-    tr_ptr = malloc(sizeof(char) * (len + 1));
-    if (tr_ptr == NULL)
-    {
-        return (NULL);
-    }
-    for (tr = 0; tr < len; tr++)
-    {
-        tr_ptr[tr] = str[tr];
-    }
-    tr_ptr[len] = '\0';
-    return (tr_ptr);
+	if (str == NULL)
+		return (NULL);
+		while (*str != '\0')
+		{
+			len++;
+			str++;
+		}
+		str = str - len;
+		tr_ptr = malloc(sizeof(char) * (len + 1));
+		if (tr_ptr == NULL)
+		{
+			return (NULL);
+		}
+		for (tr = 0; tr < len; tr++)
+		{
+			tr_ptr[tr] = str[tr];
+		}
+		tr_ptr[len] = '\0';
+		return (tr_ptr);
 }
 
+/**
+ * tr_strcmp - Compares two strings.
+ * @tr1: First string to compare.
+ * @tr2: Second string to compare.
+ *
+ * Compares two strings 'tr1' and 'tr2'.
+ *
+ * Return: 0 if strings are equal, positive value
+ */
 int tr_strcmp(char *tr1, char *tr2)
 {
-    int tr_cmp;
+	int tr_cmp;
 
 
-    tr_cmp = *tr1 - *tr2;
-    while (*tr1 == *tr2)
-    {
-        if (*tr1 == '\0')
-        {
-            return (0);
-        }
-        tr1++;
-        tr2++;
-    }
-    return (tr_cmp);
+	tr_cmp = *tr1 - *tr2;
+	while (*tr1 == *tr2)
+	{
+		if (*tr1 == '\0')
+		{
+			return (0);
+		}
+		tr1++;
+		tr2++;
+	}
+	return (tr_cmp);
 }
